8059ac98f8bd56251ff26370dbd0df14155a7497
Ghanshyam Prasad <ghanshyam@justwravel.com>
2023-07-21 14:21:15 +0530

    build version and date

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-echo "Welcome Test repo PHP | version 1.0.8";
-echo '<hr>is it working update for 1.0.8??';
-echo '<hr>Yes, it is working after version 1.0.7 to 1.0.8';
+echo "Welcome Test repo PHP | version : BUILD_VERSION | date: BUILD_DATE";
+
 
 // phpinfo();
